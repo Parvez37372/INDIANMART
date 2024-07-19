f260fbd8b91288dc5b5b41512f3aa1ab89364224
Parvez Ahmad <142169142+Parvez37372@users.noreply.github.com>
2024-07-20 01:17:57 +0530

    Add files via upload

---
+++ Slider2.js
@@ -0,0 +1,19 @@
+
+    let currentIndex = 0;
+
+const slider = document.getElementById('sliders');
+const slides = slider.children;
+const totalSlides = slides.length;
+
+function showNextSlide() {
+  currentIndex++;
+  if (currentIndex >= totalSlides) {
+    currentIndex = 0;
+  }
+  slider.style.transform = `translateX(${-currentIndex * 310}px)`;
+}
+
+setInterval(showNextSlide, 3000);
+
+
+    
